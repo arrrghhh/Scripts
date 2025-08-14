00ce6683396f8229340bf7a952e01f15d6cce1cc
arrrghhh <arrrghhh@gmail.com>
2025-08-14 11:30:09 -0400

    Create Logoff-DisconnectedUserJumpBox.ps1

---
+++ Logoff Disconnected Users/Logoff-DisconnectedUserJumpBox.ps1
@@ -0,0 +1,28 @@
+# Logoff all accounts (except JJ Moss) if they are in a 'disconnected' state (IE the user forgot to logoff)
+
+try {
+    ## Find all sessions matching the specified username
+    $sessions = quser | Where-Object {$_ -notmatch '\bjj\.moss\b'}
+    ## Initialize an array to hold session IDs
+    $sessionIds = @()
+    ## Parse the session IDs from the output
+    foreach ($session in $sessions) {
+        $sessiondisc = ($session -split ' +')[3]
+        if ($sessiondisc -eq 'Disc') {
+            $sessionId = ($session -split ' +')[2]
+            $sessionIds += $sessionId
+        }
+    }
+    Write-Host "Found $(@($sessionIds).Count) user login(s) on computer."
+    ## Loop through each session ID and pass each to the logoff command
+    $sessionIds | ForEach-Object {
+        Write-Host "Logging off session id [$($_)]..."
+        logoff $_
+    }
+} catch {
+    if ($_.Exception.Message -match 'No user exists') {
+        Write-Host "The user is not logged in."
+    } else {
+        throw $_.Exception.Message
+    }
+}
